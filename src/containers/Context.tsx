e77ebfe0a86b9bd2a9281151c2776b59ecdc71e1
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2020-09-02 00:37:28 -0400

    Add isDebouncing state

--- src/containers/Context.tsx
+++ src/containers/Context.tsx
@@ -12,18 +12,19 @@ import { ITitleData } from "../types/Title";
  */
 const Context = () => {
   const [title, setTitle] = useState("");
+  const [isDebouncing, setDebouncing] = useState(false);
   const [nominations, setNominations] = useState<Array<ITitleData>>([]);
   const [refetch, { called, loading, data: searchData }] = useLazyQuery(
     SEARCH_TITLE
   );
   const searchTimeout = useRef<number | null>(null);
-
   const onSearchChange = useCallback((newTitle: string) => {
     if (searchTimeout.current !== null)
       clearTimeout(searchTimeout.current);
     setTitle(newTitle);
-
+    setDebouncing(true);
     searchTimeout.current = window.setTimeout(() => {
+      setDebouncing(false);
       refetch({ variables: { title: newTitle } });
     }, 1500);
     
